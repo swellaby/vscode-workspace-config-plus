d837bf4d3a47aa5812849698ea91d2332de2f355
Caleb Cartwright <caleb.cartwright@outlook.com>
2021-08-16 21:17:05 -0500

    feat: add logger and tests

---
+++ src/log.js
@@ -0,0 +1,43 @@
+'use strict';
+
+let _outputChannel;
+
+const write = (msg, logLevel) => {
+  const dateString = new Date().toLocaleString('sv');
+  module.exports._outputChannel.appendLine(`[${dateString}] [${logLevel}] ${msg}`);
+}
+
+const debug = (msg) => {
+  write(msg, 'DEBUG');
+}
+
+const error = (msg) => {
+  write(msg, 'ERROR');
+}
+
+const info = (msg) => {
+  write(msg, 'INFO');
+}
+
+const warn = (msg) => {
+  write(msg, 'WARN');
+}
+
+const initialize = (createChannel) => {
+  module.exports._outputChannel = createChannel('Workspace Config+');
+}
+
+const dispose = () => {
+  module.exports._outputChannel.dispose();
+}
+
+module.exports = {
+  debug,
+  error,
+  info,
+  warn,
+  initialize,
+  dispose,
+  // Private, exported for unit testing
+  _outputChannel,
+}
